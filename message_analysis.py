40a132938a8156b4f96e7ae20f2020c6bab102da
Johnson Hsu <specialline4107@gmail.com>
2024-12-18 22:42:06 +0800

    Add the function of average length of message

--- message_analysis.py
+++ message_analysis.py
@@ -7,9 +7,15 @@ with open('reviews.txt', 'r') as f:
         data.append(line.strip())
         count += 1
         if count % 1000 == 0:
-            print('It is', count, 'th runs')
+            print('It is', count, 'th runs')      
+print('It is done running,', 'There are in total', count, 'messages')
 
-print(len(data))
-print(data[0])
-print('---------------------------------')
-print(data[1])
+
+# Calculate average message length
+length = 0
+count_d = 0
+for d in data:
+    length += len(d)
+    count_d += 1
+    if count_d == len(data):
+        print('The average length of message is', length/len(data))
