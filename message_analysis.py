c91459fd6c29a8ff94261a44c18db2d1ad323680
Johnson Hsu <specialline4107@gmail.com>
2024-12-18 22:13:20 +0800

    Add count function

--- message_analysis.py
+++ message_analysis.py
@@ -1,7 +1,15 @@
 # Message analysis
 
 data = []
+count = 0
 with open('reviews.txt', 'r') as f:
     for line in f:
-        data.append(line)
+        data.append(line.strip())
+        count += 1
+        if count % 1000 == 0:
+            print('It is', count, 'th runs')
+
 print(len(data))
+print(data[0])
+print('---------------------------------')
+print(data[1])
